a66487e7a458bdeba90d8353dba7e95f51df1e8c
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2015-05-30 12:46:09 +0200

    Initial commit

---
+++ Cassini/ImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by Mads Bielefeldt on 30/05/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController
+{
+}
---
+++ Cassini/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Cassini
+//
+//  Created by Mads Bielefeldt on 30/05/15.
+//  Copyright (c) 2015 GN ReSound. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+}
+
